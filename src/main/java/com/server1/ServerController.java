088926510d58142c7881dd2f0f096907f544acb0
sebastiano.anchise <sebastiano.anchise@CPX-RFZOA8AS7H4.dir.svc.accenture.com>
2018-07-13 17:58:52 +0200

    Big Update ServerController.java

--- src/main/java/com/server1/ServerController.java
+++ src/main/java/com/server1/ServerController.java
@@ -42,9 +42,11 @@ import redis.clients.jedis.Jedis;
 public class ServerController {
 	
 	static ArrayList<User> users = new ArrayList<User>();
+	static ArrayList<User> send2Cedacri = new ArrayList<User>();
+	static String operation = "";
 	
-	@RequestMapping(value= "request/{alberto}", method = RequestMethod.GET)
-	public ResponseEntity<String> getRequests(@PathVariable("alberto") String stato, @RequestParam("api-version") String apiVersion) throws InterruptedException, ExecutionException {
+	@RequestMapping(value= "request2insert", method = RequestMethod.GET)
+	public ResponseEntity<String> getRequests() throws InterruptedException, ExecutionException {
 		ExecutorService executor = Executors.newFixedThreadPool(10);
 		System.out.println("Avvio Retriever");
 		Future<Integer> usersRetrieved = executor.submit(retrieveRequests);
@@ -53,6 +55,7 @@ public class ServerController {
 		}
 		if(usersRetrieved.get()==2) {
 			System.out.println("Avvio Sender");
+			operation = "memorizza";
 			Future<Integer> a = executor.submit(sendRequests);
 			while(!a.isDone()) {
 				// Wait...
@@ -64,8 +67,51 @@ public class ServerController {
 		
 	}
 	
+	@RequestMapping(value= "send2cedacri/", method = RequestMethod.POST)
+	public ResponseEntity<Boolean> send2Cedacri(@RequestBody String gson){
+		System.out.println(gson);
+		System.out.println("Richiesta ben formata");
+		return new ResponseEntity<Boolean>(true,HttpStatus.OK);
+		
+	}
+
+	@RequestMapping(value= "updaterequest/", method = RequestMethod.PUT)
+	public ResponseEntity<Boolean> updateRequest(@RequestBody String gson) throws SQLException{
+		System.out.println(gson);
+		Gson g = new Gson();
+		User user = g.fromJson(gson, User.class);
+		Connection cn = null;
+		PreparedStatement ps = null;
+		int rs = 0;
+		try {
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql://172.17.0.9:5432/db";
+			cn = DriverManager.getConnection(url, "sebastiano", System.getenv("PWD_PSQL"));
+			String sql = "update users set sent2Cedacri = 1 where login = ?";
+			ps = cn.prepareStatement(sql);
+			ps.setString(1, user.getLogin());
+			rs = ps.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(cn != null) {
+				cn.close();
+			}
+			if(ps != null) {
+				ps.close();
+			}
+		}
+		
+		if(rs == 1) {
+			System.out.println("Utente aggiornato correttamente");
+			return new ResponseEntity<Boolean>(true,HttpStatus.OK);
+		}
+		return new ResponseEntity<Boolean>(false,HttpStatus.INTERNAL_SERVER_ERROR);
+		
+	}
+	
+	
 	@RequestMapping(value= "insertrequest/", method = RequestMethod.POST)
-	@ResponseBody
 	public ResponseEntity<Boolean> insertRequest(@RequestBody String gson) throws SQLException {
 		System.out.println(gson);
 		Gson g = new Gson();
@@ -104,16 +150,65 @@ public class ServerController {
 		
 	}
 	
+	@RequestMapping(value= "request2send/", method = RequestMethod.GET)
+	public ResponseEntity<String> request2send(){
+		ExecutorService es = Executors.newFixedThreadPool(10);
+		Future<Object> ft = es.submit(retrieveRequests4Cedacri);
+		while(!ft.isDone()) {
+			// Wait...
+		}
+		operation = "forward";
+		Future<Object> f = es.submit(sendRequests);
+		while(!f.isDone()) {
+			//Wait...
+		}
+		es.shutdownNow();
+
+		return new ResponseEntity<String>("Partito recupero delle richieste per invio a Cedacri",HttpStatus.ACCEPTED);
+	}
+
+	static Callable retrieveRequests4Cedacri = () -> {
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql://172.17.0.9:5432/db";
+			conn = DriverManager.getConnection(url, "sebastiano", System.getenv("PWD_PSQL"));
+			String query = "select nome,cognome,login,status from users where sent2cedacri = ?";
+			ps = conn.prepareStatement(query);
+			ps.setString(1, "0");
+			ps.setFetchSize(500);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				send2Cedacri.add(new User(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4)));				
+			}
+			send2Cedacri.trimToSize();
+			System.out.println("Dimensione send2Cedacri: " +send2Cedacri.size() + " contenuto: " +send2Cedacri.toString());
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(conn != null) { conn.close(); } 
+			if(ps != null)   { ps.close(); }
+			if(rs != null)   { rs.close(); }
+		}
+		
+		return null;
+	};
+	
+	
 	static Callable retrieveRequests = () -> {
 		Connection conn = null;
 		PreparedStatement ps = null;
+		ResultSet rs = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection("jdbc:mysql://172.30.20.211:3306/dbSeba", "sebastiano", System.getenv("MYSQL_PWD"));
 			String query = "select * from user where stato = ?";
 			ps = conn.prepareStatement(query);
 			ps.setString(1, System.getenv("Query_Param"));
-			ResultSet rs = ps.executeQuery();
+			ps.setFetchSize(500);
+			rs = ps.executeQuery();
 			while (rs.next()) {
 				System.out.println(rs.getString(1) + " " + rs.getString(2) + " " +  rs.getString(3) + " " + rs.getString(4));
 				users.add(new User(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4)));
@@ -126,6 +221,8 @@ public class ServerController {
 				conn.close();
 			if(ps != null)
 				ps.close();
+			if(rs != null) 
+				rs.close();
 		}
 		return 2;
 	};
@@ -140,14 +237,29 @@ public class ServerController {
 			
 			System.out.println("Dimensione: " + users.size());
 			System.out.println("Pusho e pubblicooooo");
-			for(int i = 0; i < users.size(); i++) {	
-				User user = users.get(i);
+			String channel = "";
+			String queue = "";
+			ArrayList<User> useIt = new ArrayList<User>();
+			if(operation.equalsIgnoreCase("memorizza")) {
+				queue = "Users";
+				channel = "Nuovi_Utenti";
+				useIt.addAll(users);
+				users = new ArrayList<User>();
+				useIt.trimToSize();
+			}else if(operation.equalsIgnoreCase("forward")) {
+				queue = "Cedacri";
+				channel = "Forward2Cedacri";
+				useIt.addAll(send2Cedacri);
+				send2Cedacri = new ArrayList<User>();
+				useIt.trimToSize();
+			}
+			for(int i = 0; i < useIt.size(); i++) {	
+				User user = useIt.get(i);
 				Gson g = new Gson();
 				String push = g.toJson(user);
-				jClient.lpush("Users", push);
-				jClient.publish("Nuovi_Utenti", "Nuovi Utenti caricati");
+				jClient.lpush(queue, push);
+				jClient.publish(channel, "Nuove Operazioni");
 			}
-			users = new ArrayList<User>();
 			jClient.disconnect();
 		}catch(Exception e) {
 			e.printStackTrace();
